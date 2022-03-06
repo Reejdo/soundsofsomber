a48b8bae721a5b1d0fc2eebc53f1aabd8e163aa2
MaximumFlyer <m.mcfeggan@comcast.net>
2022-03-06 14:33:24 -0600

    Thought Bubble Level
    added scene for the thought bubble climbing puzzle
    added enemy that shoots slow projectiles

---
+++ MAGD 488 - Sounds of Somber/Assets/Scripts/Max Scripts/BubbleEnemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleEnemy : MonoBehaviour
+{
+
+	private float timeBtwshots;
+	public float startTimeBtwShots;
+	public Transform shootPoint;
+	public GameObject projectile;
+	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBtwshots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(timeBtwshots <= 0){
+    		Instantiate(projectile, shootPoint.position, Quaternion.identity);
+    		timeBtwshots = startTimeBtwShots;
+    	} else{
+    		timeBtwshots -= Time.deltaTime;
+    	}
+    }
+}
