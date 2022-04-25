fd859f88217f97f13f67223fe7b2ab77c7534f3e
Cole <coleamilleruww@gmail.com>
2022-04-24 21:14:07 -0500

    Further updates to Cutscene
    Made cutscene fade in and out, made cutscene load the next scene and change the checkpoint number to zero. Added another test image just to see 3 images better.

--- MAGD 488 - Sounds of Somber/Assets/Scripts/Cole Scripts/CutScene/CutScene.cs
+++ MAGD 488 - Sounds of Somber/Assets/Scripts/Cole Scripts/CutScene/CutScene.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI; 
 
 public class CutScene : MonoBehaviour
 {
     public UnityEvent LoadScene;
 
-    public float timeBetweenImages;
+    public float timeBetweenImages, timeAfterLastImage = 2f;
     public GameObject[] myImages;
+    public Image myFader; 
     private LoadScene myLoadScene;
+    private DataManager myDataManager; 
     // Start is called before the first frame update
 
     private void Awake()
     {
         myLoadScene = GetComponent<LoadScene>();
+        myFader = myFader.gameObject.GetComponent<Image>();
+        myDataManager = GameObject.FindObjectOfType<DataManager>().GetComponent<DataManager>(); 
     }
 
     void Start()
@@ -42,15 +47,48 @@ public class CutScene : MonoBehaviour
         LoadScene.Invoke();
     }
 
-    IEnumerator StartCutScene()
+    IEnumerator FadeOut(float fadeTime)
+    {
+        float elapsedTime = 0.0f;
+        Color c = myFader.color;
+        while (elapsedTime < fadeTime)
+        {
+            yield return null;
+            elapsedTime += Time.deltaTime;
+            c.a = 1.0f - Mathf.Clamp01(elapsedTime / fadeTime);
+            myFader.color = c;
+        }
+    }
+
+    IEnumerator FadeIn(float fadeTime)
+    {
+        float elapsedTime = 0.0f;
+        Color c = myFader.color;
+        while (elapsedTime < fadeTime)
+        {
+            yield return null;
+            elapsedTime += Time.deltaTime;
+            c.a = Mathf.Clamp01(elapsedTime / fadeTime);
+            myFader.color = c;
+        }
+    }
+
+        IEnumerator StartCutScene()
     {
         Debug.Log("StartCutScene"); 
         for (int i = 0; i < myImages.Length; i++)
         {
+            StartCoroutine(FadeOut(1f)); 
             myImages[i].SetActive(true);
             yield return new WaitForSeconds(timeBetweenImages);
+            StartCoroutine(FadeIn(1f));
             myImages[i].SetActive(false); 
         }
+
+        yield return new WaitForSeconds(timeAfterLastImage);
+
+        //need to update in case it's above 0 if we were going directly into a chapter
+        myDataManager.lastCheckpoint = 0; 
 
         CallLoadScene();
     }
