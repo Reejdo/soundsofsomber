ab9719acb7d9ef8167f2760245f81836b5b24a23
Cole <coleamilleruww@gmail.com>
2022-05-01 11:26:57 -0500

    Chapter Two - Working on Third Puzzle and slight adjustments to previous scenes
    Working on the breathing stress puzzle, adding collectable icon for collectathon, adjusting cutscene scene for videos and adding stress block object that plays a stress sound when the player is near the stress blocks.

---
+++ MAGD 488 - Sounds of Somber/Assets/Scripts/Cole Scripts/Interactables/StressBlock.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StressBlock : MonoBehaviour
+{
+    private PlayerHealth health;
+    private AudioManager myAudioManager;
+    private MoveBlockManager myBlockManager;
+    private bool hurtPlayer = true;
+    private bool inRange = false;
+    public float stressIncreaseNum = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = GameObject.FindGameObjectWithTag("MainPlayer").GetComponent<PlayerHealth>();
+        myAudioManager = GameObject.FindObjectOfType<AudioManager>().GetComponent<AudioManager>();
+        FindBlockManager(); 
+
+        //incase breathing still playing on level reload
+        myAudioManager.StopSound("PlayerBreathing");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myAudioManager == null)
+        {
+            Debug.Log("Audio manager null"); 
+        }
+
+        if (inRange)
+        {
+            if (hurtPlayer == true)
+            {
+                hurtPlayer = false;
+                //put a variable for number because I'm using it in a context where it needs to be high
+                health.IncreaseStress(stressIncreaseNum);
+                StartCoroutine(HitCoolDown());
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("MainPlayer"))
+        {
+            inRange = true;
+            myAudioManager.Play("PlayerBreathing"); 
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("MainPlayer"))
+        {
+            inRange = false;
+            if (myBlockManager.currentBlock != gameObject)
+            {
+                myAudioManager.StopSound("PlayerBreathing");
+            }
+
+        }
+
+    }
+
+    private IEnumerator HitCoolDown()
+    {
+        yield return new WaitForSeconds(2f);
+        hurtPlayer = true;
+    }
+
+    void FindBlockManager()
+    {
+        //Debug.Log("FIND BLOCK MANAGER"); 
+
+        while (myBlockManager == null)
+        {
+            myBlockManager = GameObject.FindObjectOfType<MoveBlockManager>().GetComponent<MoveBlockManager>();
+        }
+    }
+}
