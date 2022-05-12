9e67811d660639e176f9bb627960cd7d5e3d0c09
Cole <coleamilleruww@gmail.com>
2022-05-12 10:16:19 -0500

    Final touches, added last cutscene, added dog
    Tried to make a WebGL version but it didn't work so I'm sticking to the downloadables, added last cutscene, added dog at the end of chapter two.

---
+++ MAGD 488 - Sounds of Somber/Assets/Scripts/Cole Scripts/VideoPlayerWebGL.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoPlayerWebGL : MonoBehaviour
+{
+    //public string pathName = "MenuLoop.mp4"; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        var videoPlayer = gameObject.GetComponent<UnityEngine.Video.VideoPlayer>();
+
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, pathName); 
+
+        videoPlayer.Play();
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
